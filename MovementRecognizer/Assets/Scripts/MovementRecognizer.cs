5e49b52e018353a69107ffbac96c96806f6a4159
118dg <118dg@naver.com>
2021-03-15 15:22:25 +0900

    How to Detect a Movement in VR - Unity VR Tutorial - PART 2 - 9:28

--- MovementRecognizer/Assets/Scripts/MovementRecognizer.cs
+++ MovementRecognizer/Assets/Scripts/MovementRecognizer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using PDollarGestureRecognizer;
 
 public class MovementRecognizer : MonoBehaviour
 {
@@ -12,7 +13,10 @@ public class MovementRecognizer : MonoBehaviour
 
     public float newPositionThresholdDistance = 0.05f;
     public GameObject debugCubePrefab;
+    public bool creationMode = true;
+    public string newGestureName;
 
+    private List<Gesture> trainingSet = new List<Gesture>();
     private bool isPressed = true; //에러 뜨길래 내가 변수 선언함 ㅋㅋ;
     private bool isMoving = false;
 
@@ -52,7 +56,7 @@ public class MovementRecognizer : MonoBehaviour
     {
         Debug.Log("Start Movement");
         isMoving = true;
-        positionsList.Clear();
+        positionsList.Clear(); //모션 하나 그리는거 끝났으니까 Clear
         positionsList.Add(movementSource.position); //시작 위치 저장
 
         if(debugCubePrefab) //debugCubePrefab가 존재하면
@@ -65,6 +69,33 @@ public class MovementRecognizer : MonoBehaviour
     {
         Debug.Log("End Movement");
         isMoving = false;
+
+        //Create The Gesture From The Position List
+        Point[] pointArray = new Point[positionsList.Count]; //same size with positionsList
+
+        for(int i = 0; i < positionsList.Count; i++)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(positionsList[i]); //movement의 x,y좌표(point) 추출
+            pointArray[i] = new Point(screenPoint.x, screenPoint.y, 0); //stroke가 뭐지????
+
+        }
+
+        Gesture newGesture = new Gesture(pointArray); //pointArray배열 안에 있는 point들로 이루어진 gesture
+
+        //Add a new gesture to training set
+        if (creationMode) //새로 gesture을 만들고 싶은 경우
+        {
+            newGesture.Name = newGestureName; //public으로 입력해준 newGestureName의 값을 새 gesture의 이름으로 설정
+            trainingSet.Add(newGesture); //Gesture List에 newGesture 넣기
+        }
+        //recognize
+        else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, trainingSet.ToArray()); //gesture 분류
+            //tells us which geture it is, but also the score of the recognition of this element
+
+            Debug.Log(result.GestureClass + " " + result.Score); //결과 출력!
+        }
     }
 
     void UpdateMovement()
